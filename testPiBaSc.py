c7167397de78138dc61286558712bd0b5ca1e7ac
graySquirrel <fritzebner@gmail.com>
2023-01-20 09:37:56 -0500

    first

---
+++ testPiBaSc.py
@@ -0,0 +1,61 @@
+from PickleballScore import *
+
+## this is the sequence for https://www.youtube.com/watch?v=Wmu2E3AYHrM
+## pbIslandJan16_2_2000p annotated
+pbs = PickleballScore()
+pbs.debug = True
+
+pbs.update_state(ServerSide.NR) # start
+pbs.update_state(ServerSide.FR)
+pbs.update_state(ServerSide.FL)
+pbs.update_state(ServerSide.FR)
+
+pbs.update_state(ServerSide.NR)
+pbs.update_state(ServerSide.NL)
+pbs.update_state(ServerSide.NR)
+pbs.update_state(ServerSide.NL)
+
+pbs.update_state(ServerSide.FR)
+pbs.update_state(ServerSide.FL)
+pbs.update_state(ServerSide.NR)
+pbs.update_state(ServerSide.NL)
+
+pbs.update_state(ServerSide.NR)
+pbs.update_state(ServerSide.NL)
+pbs.update_state(ServerSide.NR)
+pbs.update_state(ServerSide.FL)
+
+pbs.update_state(ServerSide.FR)
+pbs.update_state(ServerSide.NR)
+pbs.update_state(ServerSide.NL)
+pbs.update_state(ServerSide.NR)
+
+pbs.update_state(ServerSide.FR)
+pbs.update_state(ServerSide.FL)
+pbs.update_state(ServerSide.NR)
+pbs.update_state(ServerSide.NL)
+
+pbs.update_state(ServerSide.FR)
+pbs.update_state(ServerSide.FL)
+pbs.update_state(ServerSide.FR)
+pbs.update_state(ServerSide.NR)
+
+pbs.update_state(ServerSide.NL)
+pbs.update_state(ServerSide.NR)
+pbs.update_state(ServerSide.FR)
+pbs.update_state(ServerSide.FL)
+
+pbs.update_state(ServerSide.FR)
+pbs.update_state(ServerSide.FL)
+pbs.update_state(ServerSide.NR)
+pbs.update_state(ServerSide.NL)
+
+pbs.update_state(ServerSide.NR)
+pbs.update_state(ServerSide.NL)
+pbs.update_state(ServerSide.NR)
+pbs.update_state(ServerSide.NL)
+
+pbs.update_state(ServerSide.NR)
+pbs.update_state(ServerSide.NL)
+pbs.update_state(ServerSide.NR)
+
