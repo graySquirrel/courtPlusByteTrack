ab518a8b1b54a18e36ae77461b1c409f3bbf8737
graySquirrel <fritzebner@gmail.com>
2023-01-27 17:54:11 -0500

    first

---
+++ reProcessAll.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -i
+
+./processIt ~/Videos/dinkersJan21_0_2000p.mp4 1 1
+./processIt ~/Videos/dinkersJan21_1_2000p.mp4 1 1
+./processIt ~/Videos/dinkersJan21_2_2000p.mp4 1 1
+
+./processIt ~/Videos/pbIslandJan16_1_2000p.mp4 1 1
+./processIt ~/Videos/pbIslandJan16_2_2000p.mp4 1 1
+./processIt ~/Videos/pbIslandJan16_3_2000p.mp4 1 1
+
+./processIt ~/Videos/senProf22.mp4 1 0
+
+./processIt ~/Videos/tuesOneGame.mp4 1 0
+
+./processIt ~/Videos/pbIslandJan8_1_HEVC_1187p.mp4 1 0
+
+
