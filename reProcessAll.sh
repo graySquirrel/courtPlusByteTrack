ef874758c23b46b6b034f4747c7732200c429a8d
graySquirrel <fritzebner@gmail.com>
2023-02-01 15:50:52 -0500

    broke processing into 2 parts for ease

--- reProcessAll.sh
+++ reProcessAll.sh
@@ -1,17 +1,30 @@
 #!/bin/bash -i
 
-./processIt.sh ~/Videos/dinkersJan21_0_2000p.mp4 1 1
-./processIt.sh ~/Videos/dinkersJan21_1_2000p.mp4 1 1
-./processIt.sh ~/Videos/dinkersJan21_2_2000p.mp4 1 1
+#./processIt_justBBs.sh ~/Videos/dinkersJan21_0_2000p.mp4 1 1 #annotated uploaded
+#./processIt_justBBs.sh ~/Videos/dinkersJan21_1_2000p.mp4 1 1 #annotated uploaded
+#./processIt_justBBs.sh ~/Videos/dinkersJan21_2_2000p.mp4 1 1 #annotated uploaded
 
-./processIt.sh ~/Videos/pbIslandJan16_1_2000p.mp4 1 1
-./processIt.sh ~/Videos/pbIslandJan16_2_2000p.mp4 1 1
-./processIt.sh ~/Videos/pbIslandJan16_3_2000p.mp4 1 1
+#./processIt_justBBs.sh ~/Videos/pbIslandJan16_1_2000p.mp4 1 1 #annotated uploading
+#./processIt_justBBs.sh ~/Videos/pbIslandJan16_2_2000p.mp4 1 1
+#./processIt_justBBs.sh ~/Videos/pbIslandJan16_3_2000p.mp4 1 1
 
-./processIt.sh ~/Videos/senProf22.mp4 1 0
+#./processIt_justBBs.sh ~/Videos/senProf22.mp4 1 0
 
-./processIt.sh ~/Videos/tuesOneGame.mp4 1 0
+#./processIt_justBBs.sh ~/Videos/tuesOneGame.mp4 1 0
 
-./processIt.sh ~/Videos/pbIslandJan8_1_HEVC_1187p.mp4 1 0
+#./processIt_justBBs.sh ~/Videos/pbIslandJan8_1_HEVC_1187p.mp4 1 0
 
+./processIt_OtherPart.sh ~/Videos/dinkersJan21_0_2000p.mp4 1 1 
+./processIt_OtherPart.sh ~/Videos/dinkersJan21_1_2000p.mp4 1 1 
+./processIt_OtherPart.sh ~/Videos/dinkersJan21_2_2000p.mp4 1 1
+
+./processIt_OtherPart.sh ~/Videos/pbIslandJan16_1_2000p.mp4 1 1 
+./processIt_OtherPart.sh ~/Videos/pbIslandJan16_2_2000p.mp4 1 1
+./processIt_OtherPart.sh ~/Videos/pbIslandJan16_3_2000p.mp4 1 1
+
+./processIt_OtherPart.sh ~/Videos/senProf22.mp4 1 0
+
+./processIt_OtherPart.sh ~/Videos/tuesOneGame.mp4 1 0
+
+./processIt_OtherPart.sh ~/Videos/pbIslandJan8_1_HEVC_1187p.mp4 1 0
 
